9c3d9e27c65bb545040f85cd1ce7cc8224d2f5c8
yukimura1014 <yukimu.yk@gmail.com>
2022-02-09 16:29:06 +0900

    Code Change

--- searchbox.js
+++ searchbox.js
@@ -1,13 +1,3 @@
-//各サイトのURL
-let site_url = [
-    ['Google','https://www.google.co.jp/search?q=','0'],
-    ['Youtube','https://www.youtube.com/results?search_query=','0'],
-    ['Qiita','https://qiita.com/search?q=','0'],
-    ['Twitter','https://twitter.com/search?q=','0'],
-    ['Note','https://note.com/search?q=','0']
-  ]
-  
-  
 //検索処理*************************************************************************
 function searchText(site_url){//検索処理
     
@@ -92,28 +82,37 @@ function selectboxChange(site_url){
 }
 
 //メイン処理************************************************************************
+//各サイトのURL
+let site_url_1 = [
+  ['Google','https://www.google.co.jp/search?q=','0'],
+  ['Youtube','https://www.youtube.com/results?search_query=','0'],
+  ['Qiita','https://qiita.com/search?q=','0'],
+  ['Twitter','https://twitter.com/search?q=','0'],
+  ['Note','https://note.com/search?q=','0']
+]
+
 //Searchボタンにイベントを登録
 let searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click', {site_url: site_url, handleEvent: searchClick});
+searchButton.addEventListener('click', {site_url: site_url_1, handleEvent: searchClick});
   
 //テキストボックスにイベントを登録
 let searchbox = document.getElementById('searchText');
-searchbox.addEventListener('keypress', {site_url: site_url, handleEvent: keyEnter});
+searchbox.addEventListener('keypress', {site_url: site_url_1, handleEvent: keyEnter});
 
 //設定リンクにイベントを登録
 let settinglink = document.getElementById('settingLink');
-settinglink.addEventListener('click', {site_url:site_url, handleEvent: settingClick})
+settinglink.addEventListener('click', {site_url:site_url_1, handleEvent: settingClick})
     
 //セレクトボックスの値を設定
 let searchSite = document.getElementById('searchSite');
   
-for (let i=0; i < site_url.length; i++){
+for (let i=0; i < site_url_1.length; i++){
   //option要素を新しく作る
   let optionSite = document.createElement('option');
   
   //option要素にvalueと表示名を設定
-  optionSite.value = site_url[i][1];//設定値
-  optionSite.textContent = site_url[i][0];//表示名
+  optionSite.value = site_url_1[i][1];//設定値
+  optionSite.textContent = site_url_1[i][0];//表示名
     
   //select要素にoption要素を追加する
   searchSite.appendChild(optionSite);
