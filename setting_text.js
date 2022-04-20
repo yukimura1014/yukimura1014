3e3370a30824237f46a485a343540e2fc0cf3717
yukimura1014 <yukimu.yk@gmail.com>
2022-04-20 17:52:38 +0900

    テキスト方式の設定ページお試し

---
+++ setting_text.js
@@ -0,0 +1,42 @@
+const Setting_File = 'default.txt'
+
+function setValue(file_path,viewId){
+    //設定ファイルをオープン
+    const xhr = new XMLHttpRequest();
+    //取得するファイルの設定
+    xhr.open('get', file_path);
+    xhr.send();
+  
+    xhr.onreadystatechange = function() {
+        //通信が正常に完了したか確認
+        if( xhr.readyState === 4 && xhr.status === 200) {
+            //ファイルの値をテキストボックスに表示
+            var text_data = xhr.responseText;
+            //指定の場所に表示
+            document.getElementById(viewId).innerHTML = text_data;
+        }
+    }
+}
+
+function saveClick(){
+    
+    //テキストデータの読み込み
+
+    //ファイルに出力
+    var blob =new Blob([str],{type:"text/csv"}); //配列に上記の文字列(str)を設定
+    var link =document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download ="default.txt";
+    link.click();
+    
+}
+
+
+window.onload = function() {
+    //Saveボタンにイベントを登録
+    let saveButton = document.getElementById('saveButton');
+    saveButton.addEventListener('click', saveClick);  
+  
+    //設定ファイルをテーブルへ反映
+    setValue(Setting_File,"Edit_Text");
+}
